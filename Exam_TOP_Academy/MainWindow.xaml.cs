f4e832671299bdd928b36905f0bff2abad235067
thisfozz <kfcfozz@vk.com>
2024-02-05 00:20:25 +0500

    Начал делать форму авторизации.

--- Exam_TOP_Academy/MainWindow.xaml.cs
+++ Exam_TOP_Academy/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace Exam_TOP_Academy;
 
@@ -7,5 +8,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void MinButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
+    }
+
+    private void ExitButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void ToolBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        if(e.ChangedButton == MouseButton.Left)
+        {
+            this.DragMove();
+        }
     }
 }
