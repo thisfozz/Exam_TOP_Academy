614305c07d0f9865b52c8081f10befb1d7fe0502
thisfozz <kfcfozz@vk.com>
2024-02-06 20:01:26 +0500

    /

--- Exam_TOP_Academy/ViewModels/AuthorizationViewModel.cs
+++ Exam_TOP_Academy/ViewModels/AuthorizationViewModel.cs
@@ -9,19 +9,40 @@ public class AuthorizationViewModel
 {
     public ICommand OpenSettingsButtonCommand { get; }
     public ICommand AuthorizationButtonCommand { get; }
+    public ICommand CloseFormCommand { get; }
+    public ICommand RollUpFormCommand { get; }
+    public ICommand MovingFormCommand { get; }
     public AuthorizationViewModel()
     {
         OpenSettingsButtonCommand = new DelegateCommand(OpenSettings, _ => true);
         AuthorizationButtonCommand = new DelegateCommand(Authorization, _ => true);
+        CloseFormCommand = new DelegateCommand(CloseForm, _ => true);
+        RollUpFormCommand = new DelegateCommand(RollUpForm, _ => true);
+        MovingFormCommand = new DelegateCommand(MovingForm, _ => true);
+    }
+
+    private void MovingForm(object obj)
+    {
+        MessageBox.Show("Перемещение");
+    }
+
+    private void RollUpForm(object obj)
+    {
+        MessageBox.Show("Окно свернуто");
+    }
+
+    private void CloseForm(object obj)
+    {
+        Application.Current.Shutdown();
     }
 
     private void Authorization(object obj)
     {
-        throw new NotImplementedException();
+        MessageBox.Show("Успех авторизации");
     }
 
     private void OpenSettings(object obj)
     {
-        throw new NotImplementedException();
+        MessageBox.Show("Настройки");
     }
 }
